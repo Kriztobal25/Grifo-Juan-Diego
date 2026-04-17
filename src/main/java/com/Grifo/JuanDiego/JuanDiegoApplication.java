23e066e786e24b605b34e1ce5d9c07f8c0b9fb48
Kriztobal25 <jackstauberteamo111@gmail.com>
2026-04-17 13:44:36 -0500

    Commit Grifo JD

---
+++ src/main/java/com/Grifo/JuanDiego/JuanDiegoApplication.java
@@ -0,0 +1,13 @@
+package com.Grifo.JuanDiego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanDiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanDiegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Grifo/JuanDiego/JuanDiegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grifo.JuanDiego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanDiegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
